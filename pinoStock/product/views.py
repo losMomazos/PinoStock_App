b614cdbba13ed094e7581ba236afa99da583c5e6
Francisco Antilef <francisco.antilef.jaramillo@gmail.com>
2018-04-27 03:53:58 -0300

    renderizado de datos en los templates:basico

--- pinoStock/product/views.py
+++ pinoStock/product/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
+from .models import MotherBoard
 # Create your views here.
 def hello_world(request):
-	return render(request,'index.html')
+	motherboard  = MotherBoard.objects.order_by('id')
+
+	template = loader.get_template('motherboard.html')
+
+	context = {
+		'motherboard':motherboard
+	}
+	return HttpResponse(template.render(context,request))
+	#return render(request,'motherboard.html')
